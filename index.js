ff35ec0605ea75d219f160cc1ea6ec4d3d4590d5
Mark Franciose <mark.franciose@gmail.com>
2020-11-29 12:21:47 -0600

    Add 'Hello World' w/ socket.io
    - Add socket.io && update dependencies
    - Require CDN-hosted socket-io client-side
    - Create basic socket.io function to log connections

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 var app = require('express')();
 var http = require('http').createServer(app);
+var sockets = require('socket.io')(http);
 
 app.get('/', function serverResponse(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
+
+sockets.on('connection', function socketHW(socket) {
+  console.log('we connect');
+})
 
 http.listen(3001, function serverListener() {
   console.log('listening on *:3001');
